281fb603c6b0d4fe012dcab50add4a60c6e7a58b
Aly <alyconr@hotmail.com>
2025-03-01 14:09:12 -0500

    :tada: subiendo yaml

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     index: './src/main.js',
-     style: './src/assets/styles.css',
-     
-   },
-   devtool: 'inline-source-map',
-   devServer: {
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/main.js',
+    style: './src/assets/styles.css',
+
+  },
+  devtool: 'inline-source-map',
+  devServer: {
     static: './dist',
     hot: true,
     liveReload: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-     template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
-   module: {
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-        
+
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -52,12 +52,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
             ],
           },
         },
-      },     
-      
+      },
+
     ],
   },
   optimization: {
     runtimeChunk: 'single',
   },
- };
- 
+};

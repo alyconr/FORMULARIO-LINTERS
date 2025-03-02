c8c9d7c02f7b00da315b35974d383742b2193e08
Aly <alyconr@hotmail.com>
2025-03-01 20:36:50 -0500

    Ajuste en workflow para despliegue

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 // Serve static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// For any request that doesn't match a static file, serve index.html
+// For any request that doesn't match a static file, serve index.html  
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+// Expose 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
